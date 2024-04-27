fa6069bb4912ada937da38be3976e6a0021495c5
suqlain2544 <44050255+suqlain2544@users.noreply.github.com>
2024-04-27 14:17:46 +0000

    conversion of data types

---
+++ basics/conversion_operations.js
@@ -0,0 +1,20 @@
+let number = ""
+
+console.log(number)
+console.log(typeof number)
+let valueInNumber = Number(number)
+console.log (valueInNumber)
+console.log(typeof valueInNumber)
+//let string = ""
+//let valuInNumber= Number(string)
+//console.log(typeof valuInNumber)
+//console.log(valuInNumber)
+//conversion of "" to number  0
+// "31adc" to number NaN(not a number ) but still its type is number 
+// true in number is 1
+// false in number is 0
+//undefined in number is NaN
+// null in number is 0
+// null in string is null but type of null is string
+// undefined in string is undefined but type of undefined is still string 
+// when string is "" means empty than its convert into false in bolean vice versa 
