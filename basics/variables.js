5fea18247ea48ff2e7ffe261c90164d5d4e188f7
suqlain2544 <44050255+suqlain2544@users.noreply.github.com>
2024-04-27 05:26:48 +0000

    variables done

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountid ="1234"
+let accountemail = "mano@gmail.com"
+var accountpassword = "mano123"
+accountcity = "wazirabad"
+let accountstate;
+//accountid = "2"       this is comment//
+/*  var variable is not recommended due to issues in block scope and functional scope (scope means for loop or if else {})*/
+console.log(accountid);
+console.table([accountemail,accountid,accountpassword,accountcity,accountstate]
+);
